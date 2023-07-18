5bd13b5a189db3d6e96afcc6f46e8850e2590a7b
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-07-18 11:23:52 +0300

    rendered a tags to navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,18 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const link = links.map((linkItem) => {
+    return (
+      <li key={linkItem}>
+        <a href={`#${linkItem}`}>{linkItem}</a>
+      </li>
+    );
+  });
+  return (
+    <nav>
+      <ol style={{ display: "flex", listStyle: "none" }}>{link}</ol>
+    </nav>
+  );
 }
 
 export default NavBar;
